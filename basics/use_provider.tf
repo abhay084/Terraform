ec7e071769478b124c6b40736b97c1b24568669d
abhay084 <abhayganvir03@gmail.com>
2022-05-28 18:23:24 +0530

    fs

--- basics/use_provider.tf
+++ basics/use_provider.tf
@@ -18,4 +18,10 @@ provider "aws"{
 	region = "ap-south-1"
 	access_key = "my_aws_access_key"
 	secret_key = "my_aws_secret_key"
+
+	/* 
+	you can get your access key and secret key in aws from here:
+	AWS --> Service -->  IAM -->  Users --> Add users --> username --> next --> power_user_policy --> next --> finish.
+	
+	*/
 }
